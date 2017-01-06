468bd57c02c512cd01c06955762e8806c721f777
Peter Thompson <peter.thompson@radimetrics.com>
2017-01-06 16:23:44 -0500

    initial commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,34 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+# make CacheMatrix should return something like a matrix (a new object of some sort). When this object is passed to cacheSolve, cachesolve should return the inverse of the matrix.
+
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+
+	thematrix<<-x
+	theInverse<<-NULL
+	get<-function()
+	{
+		return(thematrix)
+	}
+	
+	set<-function(y=matrix())
+	{
+		thematrix<<-y
+	}
+	
+	getInverse<-function()
+	{
+		return(theInverse)
+	}
+	
+	setInverse<-function(y=matrix)
+	{
+		theInverse<<-y
+	}
+	return c(get,set,getInverse,setInverse)
 
 }
 
@@ -12,4 +37,5 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+		if
 }
